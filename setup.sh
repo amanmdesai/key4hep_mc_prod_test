f2ae4f8c358a2ca34ca7658a9f6ad1d3107fbe7c
amanmdesai <amanmukeshdesai@gmail.com>
2024-10-10 10:11:17 +0200

    add files

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /eos/user/a/aman/test_key4hep
+#source /cvmfs/sw.hsf.org/spackages7/key4hep-stack/2023-04-08/x86_64-centos7-gcc11.2.0-opt/urwcv/setup.sh
+
+source /cvmfs/sw.hsf.org/key4hep/setup.sh
+
+mg5_aMC madgraphrun.dat
+gunzip -c background_qq/Events/run_01/unweighted_events.lhe.gz > background_qq.lhe
+DelphesPythia8_EDM4HEP card_IDEA.tcl edm4hep_IDEA.tcl pythia_card.cmd background_qq/background_qq.root
