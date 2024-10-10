3a238fcaa8338b9e3a5d58d2296902831a91d65b
amanmdesai <amanmukeshdesai@gmail.com>
2024-10-10 16:40:55 +0200

    update

--- run.sh
+++ run.sh
@@ -7,4 +7,5 @@ source /cvmfs/sw.hsf.org/key4hep/setup.sh
 
 mg5_aMC madgraphrun.dat
 gunzip -c background_qq/Events/run_01/unweighted_events.lhe.gz > background_qq.lhe
-DelphesPythia8_EDM4HEP card_IDEA.tcl edm4hep_IDEA.tcl pythia_card.cmd background_qq/background_qq.root
+DelphesPythia8_EDM4HEP card_IDEA.tcl edm4hep_IDEA.tcl pythia_card.cmd Samples/background_qq.root
+fccanalysis run analysis_stage1.py
